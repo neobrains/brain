6d04ff183c0b72d0f42c8c0496c84560692cd1ce
jnsougata <jnsougata@gmail.com>
2023-04-04 16:55:07 +0530

    Initial commit

---
+++ brain.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+check_sudo() {
+  if ! sudo -v >/dev/null 2>&1; then
+    echo "Error: You need to have sudo privileges to install brain"
+    exit 1
+  fi
+}
+
+echo "Installing brain..."
+
+if [ -x "$(command -v nb)" ]; then
+  latest_version=$(curl -s https://api.github.com/repos/neobrains/brain/releases/latest | grep tag_name | cut -d '"' -f 4)
+
+  if nb --version | grep -q "$latest_version"; then
+    echo "brain is already installed and up-to-date."
+    exit 0
+  else
+    read -p "brain is already installed, but there is a new version available. Do you want to update it? (y/n) " answer
+    if [[ $answer =~ ^[Yy]$ ]]; then
+      curl -o brain https://raw.githubusercontent.com/neobrains/brain/$latest_version/brain.sh
+      chmod +x brain
+      check_sudo
+      sudo mv brain /usr/local/bin/
+      echo "brain has been updated."
+    else
+      echo "Installation aborted."
+      exit 0
+    fi
+  fi
+else
+  latest_version=$(curl -s https://api.github.com/repos/example/nb/releases/latest | grep tag_name | cut -d '"' -f 4)
+  curl -o brain https://raw.githubusercontent.com/neobrains/brain/$latest_version/brain.sh
+  chmod +x nb
+  check_sudo
+  sudo mv brain /usr/local/bin/
+  echo "brain installed."
+fi
+
+exit 0
